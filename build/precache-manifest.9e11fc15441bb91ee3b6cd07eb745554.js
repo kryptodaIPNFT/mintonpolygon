f65195c8f74753c01e8a8edc79c791e541489e71
kryptodaIPNFT <91292374+kryptodaIPNFT@users.noreply.github.com>
2021-09-30 00:12:18 +0100

    first commit

---
+++ build/precache-manifest.9e11fc15441bb91ee3b6cd07eb745554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a116c11d97579ec33228ecb10e0f5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f7433f5357c4d43a941",
+    "url": "./static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "10673c4c6f5bff67e1fd",
+    "url": "./static/css/main.836ecd7b.chunk.css"
+  },
+  {
+    "revision": "8f7433f5357c4d43a941",
+    "url": "./static/js/2.205b089e.chunk.js"
+  },
+  {
+    "revision": "2fbbe0115f2ab39efb2c960ccb3cb05d",
+    "url": "./static/js/2.205b089e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10673c4c6f5bff67e1fd",
+    "url": "./static/js/main.f5941afc.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
